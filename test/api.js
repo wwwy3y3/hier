9595ccfc7d353875f90b1523a9c1382a94882fc6
william <wwwy3y3@gmail.com>
2015-06-26 15:46:45 +0800

    tests use mocha

--- test/api.js
+++ test/api.js
@@ -5,39 +5,18 @@ var MemoryStore= require('../lib/memoryStore');
 var path= require('path');
 var should= require('should');
 
-/*
-// instance
-var filePath= path.resolve(__dirname, 'store.json');
-var fileStore= new FileStore(filePath);
-var memoryStore= new MemoryStore();
-
-// set values
-memoryStore.set('player', 'mj');
-fileStore.set('player', 'frank')
-.then(function () {
-	return fileStore.set('pg', 'cp3')
-})
-
-.then(function () {
-	// chain of read
-	var opts= { writeMissing: true };
-	return store.chain([ memoryStore, fileStore ]).read('pg', opts)
-})
-
-.then(function (value) {
-	console.log(value);
-	console.log(memoryStore);
-}).catch(console.error)
-*/
-
 describe('api', function () {
 	var filePath= path.resolve(__dirname, 'store.json');
 	var fileStore= new FileStore(filePath);
 	var memoryStore= new MemoryStore();
+	var otherMemoryStore= new MemoryStore();
 
 	before(function() {
 		// clear the store.json first
-		return fileStore.drop();
+		return fileStore.drop()
+		.then(function () {
+			return fileStore.set('mvp', 'curry')
+		})
 	})
 
 	describe('#MemoryStore', function () {
@@ -49,6 +28,58 @@ describe('api', function () {
 
 		it('should get value', function () {
 			memoryStore.get('where').should.equal('memory');
+		})
+	})
+
+	describe('#fileStore', function () {
+		it('should set value', function () {
+			return fileStore.set('where', 'file')
+
+			.then(function () {
+				return fileStore.get('where');
+			})
+
+			.then(function (value) {
+				value.should.equal('file');
+			})
+		})
+
+		it('should get value', function () {
+			return fileStore.get('where')
+			.then(function (value) {
+				value.should.equal('file');
+			})
+		})
+	})
+
+	describe('#chain', function () {
+		it('should get value from memoryStore', function () {
+			return store.chain([ memoryStore, fileStore ]).read('where')
+			.then(function (value) {
+				value.should.equal('memory');
+			})
+		})
+
+		it('should get value from fileStore', function () {
+			// mvp value only exist in fileStore
+			return store.chain([ memoryStore, fileStore ]).read('mvp')
+			.then(function (value) {
+				(memoryStore.get('mvp') === null).should.be.true;
+				value.should.equal('curry');
+			})
+		})
+
+		it('should cache values in stores under opts.writeMissing', function () {
+			var opts= { writeMissing: true };
+			return store.chain([ memoryStore, otherMemoryStore, fileStore ])
+			.read('mvp', opts)
+			.then(function (value) {
+				value.should.equal('curry');
+
+				// cache
+				memoryStore.get('mvp').should.equal('curry');
+				otherMemoryStore.get('mvp').should.equal('curry');
+			})
 		})
 	})
 })
