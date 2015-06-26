30d3ab522c1c2e843b6061e5b031c8baca310066
william <wwwy3y3@gmail.com>
2015-06-26 16:34:34 +0800

    add virtualStore delay time in set/get

---
+++ test/virtualStore.js
@@ -0,0 +1,33 @@
+var store= require('../');
+var Store= store.interface;
+var util= require('util');
+var Q= require('q');
+
+var VirtualStore= function () {
+	// MemoryStore
+	var self= this;
+	self.stores= {};
+	Store.call(self);
+}
+
+util.inherits(VirtualStore, Store);
+
+VirtualStore.prototype.get = function(key) {
+	var self= this;
+	return Q.delay(100).then(function () {
+		return self.stores[key] || null;
+	})
+};
+
+VirtualStore.prototype.set = function(key, value, opts) {
+	var self= this;
+	return Q.delay(100).then(function () {
+		self.stores[key]= value;
+	})	
+};
+
+VirtualStore.prototype.destroy = function(key) {
+	delete self.stores[key];
+};
+
+module.exports= VirtualStore;
