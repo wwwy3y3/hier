2332f2ff43afd6ff1324f770d6f72bc228969ffb
william <wwwy3y3@gmail.com>
2015-06-26 09:45:58 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// chain
+exports.chain= require('./lib/chain');
+
+// memory store
+exports.MemoryStore= require('./lib/memoryStore');
+
+// store interface
+exports.interface= require('./lib/store');
